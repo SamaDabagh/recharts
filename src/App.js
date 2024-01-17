ea2c1b015e2939d379f5c7110b648d6d7a5ec264
“NasrinNazariHashemi” <nasrin.nh@gmail.com>
2024-01-17 14:14:09 -0500

    Fixed tooltip and ...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
-import Example from "./Example";
+// import Example from "./Example";
 import { useEffect, useState } from "react";
 import Papa from "papaparse";
-import profiles_input_node_2_3 from "./assets/profiles_input_node_2_3.csv";
+import profiles_input_node_2_3 from "./assets/profiles_input_node_2_30.csv";
 import Profiles from "./Profiles";
 function App() {
   const [rowData, setRowData] = useState([]);
@@ -13,28 +13,37 @@ function App() {
         download: true,
         delimiter: ",",
         complete: (result) => {
-          setRowData(
-            result.data
-              .slice(1, result.data.length - 1)
-              .map((dataOfEachRow) => {
-                const rowObject = {};
-                result.data[0].forEach((key, index) => {
-                  if (index === 0) {
-                    rowObject["name"] = dataOfEachRow[index];
-                  } else rowObject[key] = Number(dataOfEachRow[index]);
-                });
-                return rowObject;
-              })
-          );
+          const finalData = result.data
+            .slice(1, result.data.length - 1)
+            .map((dataOfEachRow) => {
+              const rowObject = {};
+              result.data[0].forEach((key, index) => {
+                if (index === 0) {
+                  rowObject["name"] = dataOfEachRow[index];
+                } else {
+                  rowObject[key] = Number(dataOfEachRow[index]);
+                }
+              });
+              return rowObject;
+            });
+          finalData.forEach((element, index, array) => {
+            if (!index) {
+              element["x-axis-key"] = 0;
+            } else {
+              element["x-axis-key"] =
+                array[index - 1]["x-axis-key"] + array[index - 1].Length;
+            }
+          });
+          setRowData(finalData);
         },
       });
     };
     fetchParseData();
   }, []);
-  console.log("data:", rowData);
+
   return (
     <div className="App">
-      <Example data={rowData} />
+      {/* <Example data={rowData} /> */}
       {rowData.length > 0 && <Profiles data={rowData} />}
     </div>
   );
--- src/Profiles.js
+++ src/Profiles.js
@@ -12,123 +12,15 @@ import {
   Rectangle,
 } from "recharts";
 
-// const data = [
-//   {
-//     name: "Page A",
-//     UCE: 4000,
-//     UGE: 6400,
-//     UIE: 2000,
-//     amt: 2400,
-//     sama: "ch0",
-//   },
-//   {
-//     name: "Page B",
-//     UCE: 3000,
-//     UGE: 4398,
-//     UIE: 2000,
-//     amt: 2210,
-//     sama: "ch1",
-//   },
-//   {
-//     name: "Page C",
-//     UCE: 2000,
-//     pv: 9800,
-//     UIE: 2000,
-//     amt: 2290,
-//     sama: "ch2",
-//   },
-//   {
-//     name: "Page D",
-//     UCE: 2780,
-//     UGE: 3908,
-//     UIE: 2000,
-//     amt: 2000,
-//     sama: "ch3",
-//   },
-//   {
-//     name: "Page E",
-//     UCE: 1890,
-//     UGE: 4800,
-//     UIE: 2000,
-//     amt: 2181,
-//     sama: "ch4",
-//   },
-//   {
-//     name: "Page F",
-//     UCE: 2390,
-//     UGE: 3800,
-//     UIE: 2000,
-//     amt: 2500,
-//     sama: "ch5",
-//   },
-//   {
-//     name: "Page G",
-//     UCE: 3490,
-//     UGE: 4300,
-//     UIE: 2000,
-//     amt: 2100,
-//     sama: "ch6",
-//     "Slope (%)": 1.4,
-//   },
-// ];
-// [
-//   {
-//       "Profil": "Node 3-5",
-//       "Pipe": 2,
-//       "Unode": 3,
-//       "Dnode": 4,
-//       "UGE": 101.2,
-//       "DGE": 101.5,
-//       "Diameter": 1,
-//       "Slope (%)": 1.4,
-//       "Length": 50,
-//       "UCE": 101,
-//       "DCE": 100.4,
-//       "UIE": 100,
-//       "DIE": 99.4
-//   },
-//   {
-//       "Profil": "Node 3-5",
-//       "Pipe": 3,
-//       "Unode": 4,
-//       "Dnode": 5,
-//       "UGE": 101.5,
-//       "DGE": 0,
-//       "Diameter": 1.2,
-//       "Slope (%)": 1.6,
-//       "Length": 55,
-//       "UCE": 100.5,
-//       "DCE": 99.7,
-//       "UIE": 99.3,
-//       "DIE": 98.5
-//   },
-//   {
-//       "Profil": "\n",
-//       "Pipe": null,
-//       "Unode": null,
-//       "Dnode": null,
-//       "UGE": null,
-//       "DGE": null,
-//       "Diameter": null,
-//       "Slope (%)": null,
-//       "Length": null,
-//       "UCE": null,
-//       "DCE": null,
-//       "UIE": null,
-//       "DIE": null
-//   }
-// ]
 const CustomizedLabel = (props) => {
-  // console.log("props: ", props);
   const { x, y, index } = props;
-  return null;
-  // return (
-  // <foreignObject x={x - 40} y={y - 20} width={100} height={100}>
-  //   <div>
-  //     {data[index].UCE}-{data[index]["Slope (%)"]}
-  //   </div>
-  // </foreignObject>
-  // );
+  console.log("props[index]: ", props);
+  return (
+    <foreignObject x={x} y={y + 20} width={100} height={100}>
+      <div>{props.data[index]["Slope (%)"]}</div>
+      <div>{props.data[index].Length}</div>
+    </foreignObject>
+  );
 };
 
 // using Customized gives you access to all relevant chart props
@@ -145,7 +37,7 @@ const CustomizedRectangle = (props) => {
 
     return (
       <Rectangle
-        key={firstSeriesPoint.payload.name}
+        key={firstSeriesPoint.payload.Pipe}
         width={10}
         height={yDifference}
         x={secondSeriesPoint.x - 5}
@@ -156,104 +48,52 @@ const CustomizedRectangle = (props) => {
     );
   });
 };
-/* =======>
-seState(initialState);
-    const zoom = () => {
-      let {
-        refAreaLeft,
-        refAreaRight
-      } = zoomGraph;
-      const {
-        data
-      } = zoomGraph;
-      if (refAreaLeft === refAreaRight || refAreaRight === '') {
-        setZoomGraph(prev => ({
-          ...prev,
-          refAreaLeft: '',
-          refAreaRight: ''
-        }));
-        return;
-      }
 
-      // xAxis domain
-      if (refAreaLeft && refAreaRight && refAreaLeft > refAreaRight) [refAreaLeft, refAreaRight] = [refAreaRight, refAreaLeft];
+export const CustomTooltip = ({ active, payload, label }) => {
+  console.log("active", active);
+  console.log("payload: ", payload);
+  console.log("pipe: ", payload[0]?.payload.Pipe);
+  console.log("label", label);
 
-      // yAxis domain
-      const [bottom, top] = getAxisYDomain(refAreaLeft, refAreaRight, 'cost', 1);
-      const [bottom2, top2] = getAxisYDomain(refAreaLeft, refAreaRight, 'impression', 50);
-      setZoomGraph(prev => ({
-        ...prev,
-        refAreaLeft: '',
-        refAreaRight: '',
-        data: data?.slice(),
-        left: refAreaLeft,
-        right: refAreaRight,
-        bottom,
-        top,
-        bottom2,
-        top2
-      } as any));
-    };
-    const zoomOut = () => {
-      const {
-        data
-      } = zoomGraph;
-      setZoomGraph(prev => ({
-        ...prev,
-        data: data?.slice(),
-        refAreaLeft: '',
-        refAreaRight: '',
-        left: 'dataMin',
-        right: 'dataMax',
-        top: 'dataMax+1',
-        bottom: 'dataMin',
-        top2: 'dataMax+50',
-        bottom2: 'dataMin+50'
-      }));
-    };
-    const {
-      data,
-      left,
-      right,
-      refAreaLeft,
-      refAreaRight,
-      top,
-      bottom,
-      top2,
-      bottom2
-    } = zoomGraph;
-    return <div className="highlight-bar-charts" style={{
-      userSelect: 'none',
-      width: '100%'
-    }}>
-        <button type="button" className="btn update" onClick={() => zoomOut()}>
-          Zoom Out
-        </button>
-
-        <ResponsiveContainer minHeight={500}>
-          <LineChart width={800} height={400} data={data} onMouseDown={e => setZoomGraph(prev => ({
-          ...prev,
-          refAreaLeft: e.activeLabel
-        }))} onMouseMove={e => zoomGraph.refAreaLeft && setZoomGraph(prev => ({
-          ...prev,
-          refAreaRight: e.activeLabel
-        }))} onMouseUp={() => zoom()}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis allowDataOverflow dataKey="name" domain={left && right ? [left, right] : undefined} type="number" />
-            <YAxis allowDataOverflow domain={[bottom, top]} type="number" yAxisId="1" />
-            <YAxis orientation="right" allowDataOverflow domain={[bottom2, top2]} type="number" yAxisId="2" />
-            <Tooltip />
-            <Line yAxisId="1" type="natural" dataKey="cost" stroke="#8884d8" animationDuration={300} />
-            <Line yAxisId="2" type="natural" dataKey="impression" stroke="#82ca9d" animationDuration={300} />
-
-            {refAreaLeft && refAreaRight ? <ReferenceArea yAxisId="1" x1={refAreaLeft} x2={refAreaRight} strokeOpacity={0.3} /> : null}
-          </LineChart>
-        </ResponsiveContainer>
-      </div>;
+  if (active && payload && payload.length) {
+    return (
+      <div className="custom-tooltip">
+        <p className="label">{`${label} : ${payload[0].value}`}</p>
+        <p>Pipe: {payload[0]?.payload.Pipe}</p>
+        <div>
+          {payload.map((pld) => (
+            <div style={{ display: "inline-block", padding: 10 }}>
+              <div style={{ color: pld.fill }}>{pld.value}</div>
+              <div>{pld.dataKey}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
   }
+
+  return null;
 };
-========>*/
+
 const Profiles = ({ data }) => {
+  console.log("data in profile: ", data);
+
+  const elementMax = data.reduce((prev, next) => {
+    if (prev.UGE > next.UGE) {
+      return prev;
+    }
+    return next;
+  });
+  let maxUGE = elementMax.UGE;
+
+  const elementMin = data.reduce((prev, next) => {
+    if (prev.DIE < next.DIE) {
+      return prev;
+    }
+    return next;
+  });
+  let minDIE = elementMin.DIE;
+
   return (
     <div className="container">
       <ResponsiveContainer width="100%" height={500}>
@@ -269,20 +109,15 @@ const Profiles = ({ data }) => {
           }}
         >
           <CartesianGrid />
-          {/* <XAxis
-            dataKey="Profil"
-            type="number"
-            domain={[0, "dataMax + 1000"]}
-          /> */}
-          <XAxis dataKey="name" />
-
-          <YAxis domain={[95, 105]} />
-          <Tooltip pv="UGE" />
+          <XAxis dataKey="x-axis-key" />
+          <YAxis domain={[Math.floor(minDIE - 1), Math.ceil(maxUGE + 1)]} />
+          <Tooltip content={<CustomTooltip />} cursor={{ fill: "white" }} />
+          {/* <Tooltip /> */}
           <Legend />
           <Line
             dataKey="UGE"
             stroke="#8884d8"
-            label={<CustomizedLabel data={data} offset={20} />}
+            label={<CustomizedLabel data={data} />}
           />
           <Line dataKey="UIE" stroke="#22bb11" />
 
